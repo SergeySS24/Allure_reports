6a8097a9344d2c047f0328bcb5d3b37f3713f049
Sergey Starostin <adelaide.star@gmail.com>
2022-07-11 19:39:36 +0300

    Added fist test

---
+++ src/test/java/com/FromLecture/SelenideTest.java
@@ -0,0 +1,35 @@
+package com.FromLecture;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+    }
+
+   @Test
+    public void testGitHubIssue() {
+        open("https://github.com");
+
+       $(".header-search-input").click();
+       $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+       $(".header-search-input").submit();
+
+       $(By.linkText("eroshenkoam/allure-example")).click();
+       $(".js-repo-nav").shouldBe(Condition.visible);
+       $(By.partialLinkText("Issues")).click();
+       $("#issue_76_link").click();
+       //$(withText("#76")).click();
+    }
+}
